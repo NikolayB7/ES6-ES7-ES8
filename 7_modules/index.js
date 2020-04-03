5e25abdc6f5a25048871da8ea07331a1c5a8b16c
Biletskiy <biletskiyna@gmail.com>
2020-04-03 19:27:22 +0300

    7 - impost/expost

---
+++ 7_modules/index.js
@@ -0,0 +1,15 @@
+// import MyLog,{COLOR,compute} from './module'
+
+// compute(1,2);
+// console.log(COLOR);
+// MyLog.log();
+
+
+// ------------------------------2й  вариан импорта ----------------------
+
+import * as Module from './module'
+
+Module.COLOR
+Module.compute
+Module.default.log
+
---
+++ 7_modules/module.js
@@ -0,0 +1,13 @@
+const privateVariable = 42
+
+export const COLOR = '#FFF000'
+
+export function compute(a, b) {
+    return a + b
+}
+
+export default {
+    log(){
+        console.log(privateVariable);
+    } 
+}
