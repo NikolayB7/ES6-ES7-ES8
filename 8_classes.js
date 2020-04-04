661dea1297b69ab317ef75cdf44208547373b538
Biletskiy <biletskiyna@gmail.com>
2020-04-04 20:10:39 +0300

    getter/setter

--- 8_classes.js
+++ 8_classes.js
@@ -27,6 +27,23 @@ class Programmer extends Person {
         this._job = job
     }
 
+    get job(){
+        return this._job.toUpperCase();
+    }
+
+    set job(job){
+        //В сеттерах как правило пишут валидацию
+
+        if(job.length < 2){
+            // throw new Error('Validation falied')
+            console.log('Validation falied');
+        }else{
+            this._job = job
+        }
+
+    }
+
+
     great(){
         super.great();
         console.log('Rewritten');
@@ -36,5 +53,41 @@ class Programmer extends Person {
 
 const frontend = new Programmer('Ted','Frontend')
 
-console.log(frontend);
-frontend.great()
+// console.log(frontend);
+// frontend.great()
+
+console.log(frontend.job);
+// frontend.job = '1'
+// console.log(frontend.job);
+
+frontend.job = 'Backend'
+console.log(frontend.job);
+
+//Static methods
+
+class Util {
+    average(...args){
+return args.reduce((acc,i)=> acc += i,0)/args.length
+    }
+}
+
+// Просто обратится к Util.average у нас не получится тк он не доступен
+
+const util = new Util()
+
+console.log(util.average(1,2,3,5,8,13));
+
+
+// Но если мы сделаем метод статическим то можем обратится через Util.average
+
+class Util2 {
+  static average(...args){
+return args.reduce((acc,i)=> acc += i,0)/args.length
+    }
+}
+
+const res = Util2.average(1,2,3,5,8,13);
+
+console.log(res);
+
+
