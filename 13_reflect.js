903b51e1b3574dce07347273a1a1526edfdc478e
Biletskiy <biletskiyna@gmail.com>
2020-04-06 15:52:05 +0300

    reflect

--- 13_reflect.js
+++ 13_reflect.js
@@ -15,7 +15,22 @@ class Student {
 
 // или использовать Reflect
 
+// class ProtoStudent {
+//     university = "Oxford"
+// }
+
 const student = Reflect.construct(Student,['John'])
 //первый параметр таргет функция в нашем случае это класс Student
 //второй параметр массив аргументов которые попадут в конструктор
 console.log(student);
+
+
+// Reflect.apply(student.great,{name:'Tester'},[])
+// console.log(Reflect.ownKeys(student));  //name
+
+Reflect.preventExtensions(student);
+
+student.age = 25;
+console.log(student);   //name: "John", поле age не добавилось т.к. мы запретили изменение обьекта ранее через  preventExtensions
+
+console.log(Reflect.isExtensible(student));
