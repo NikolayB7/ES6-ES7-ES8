4d4a382dfd9613d75951f56f6f1746bb68971926
Biletskiy <biletskiyna@gmail.com>
2020-04-06 12:37:21 +0300

    promises

--- 11_promises.js
+++ 11_promises.js
@@ -51,6 +51,76 @@ delay(1000)
 
     */
 
-    async function asyncDelay(){
-        await dela
-    }
+
+    /*
+//resolve async
+const delay = ms => new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`Done! ${ms}`), ms);
+})
+
+async function asyncDelay() {
+   const data =  await delay(2000)
+    console.log(data);
+    
+} 
+
+asyncDelay()    //Done! 2000
+
+*/
+
+
+/*
+//reject async
+const delay = ms => new Promise((resolve, reject) => {
+    setTimeout(() => reject(`Done! ${ms}`), ms);
+})
+
+async function asyncDelay() {
+   try{
+    const data =  await delay(2000)
+    console.log(data);
+   }catch(e){
+       console.log('Error',e);
+       
+   }
+    
+} 
+
+asyncDelay()    //Error Done! 2000
+*/
+
+
+// ---------------------------------------------------------------------------------
+
+/*
+const delay = ms => new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`Done! ${ms}`), ms);
+})
+
+//Promise.all ждет пока выполнятся все промисы, и выдаст результат массивом
+
+Promise.all([
+    delay(1000),
+    delay(500),
+    delay(2000)
+]).then( data => console.log(data));
+*/
+
+// ---------------------------------------------------------------------------------
+
+
+
+const delay = ms => new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`Done! ${ms}`), ms);
+})
+
+//Promise.race ждет пока выполнятся самы быстрый из промисов
+
+Promise.race([
+    delay(1000),
+    delay(500),
+    delay(2000)
+]).then( data => console.log(data));
+
+
+// Promise.race и Promise.all - удобно использовать для загрузки данных с сервера
